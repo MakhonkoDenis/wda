42121e1098839e44432859507ddbfbb9b3165503
shin <shin@templatemonster.me>
2017-02-01 11:57:17 +0200

    fix: sass graph file url reg

--- tools/sass-inheritance.js
+++ tools/sass-inheritance.js
@@ -7,21 +7,23 @@ module.exports = function sassInheritance( path, file, scssSource ) {
 		key, importedBy;
 
 	for ( key in files.index ) {
-		file = file.replace( /\//g, '\\' )
+		file = file.replace( /\/|\\/g, '\\' );
+		key = key.replace( /\/|\\/g, '\\' );
+
 		if ( file !== key){
 			continue;
 		}
 
 		importedBy = files.index[key].importedBy;
-		if ( 0 !== importedBy.length ){
+		if ( 0 !== importedBy.length ) {
 			return sassInheritance( path, importedBy[0], scssSource );
-		}else{
+		} else {
 			let data = { input: file };
 
 			for ( let files of scssSource ) {
-				let sourceFiles  = path + files.input;
+				let sourceFiles = path + files.input;
 
-				sourceFiles = sourceFiles.replace( /\//g, '\\' );
+				sourceFiles = sourceFiles.replace( /\/|\\/g, '\\' );
 
 				if ( sourceFiles === file ) {
 					data.output = files.output;
